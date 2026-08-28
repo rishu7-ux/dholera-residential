f5814172c2579d8147b624b41211b9284ef8cacf
rishu mishra <rishumishra694@gmail.com>
2026-08-29 00:11:37 +0530

    Fix Payload admin startup

--- app/(payload)/admin/importMap.js
+++ app/(payload)/admin/importMap.js
@@ -1,6 +1,7 @@
 import { default as default_d2ac38b21954ff08d5afd41c3589e739 } from '../../../components/admin/Estate2NavLink.tsx'
 import { default as default_1cd5156423ff10ae71c96eaddb0be83a } from '../../../components/admin/DholeraEstatesNavLink.tsx'
 import { default as default_16641a84a1b15c8d66ac7b55d014767f } from '../../../components/admin/IndustrialNavLink.tsx'
+import { VercelBlobClientUploadHandler as VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e } from '@payloadcms/storage-vercel-blob/client'
 import { default as default_1b588679deafe4663065ec587b5c83cf } from '../../../components/admin/Estate2Dashboard.tsx'
 import { default as default_d91c6f7e463ebcc16c96f2acdf19f207 } from '../../../components/admin/DholeraEstatesDashboard.tsx'
 import { default as default_ff499614a2df010e10c2f729b61add75 } from '../../../components/admin/IndustrialDashboard.tsx'
@@ -12,6 +13,7 @@ export const importMap = {
   "/components/admin/Estate2NavLink.tsx#default": default_d2ac38b21954ff08d5afd41c3589e739,
   "/components/admin/DholeraEstatesNavLink.tsx#default": default_1cd5156423ff10ae71c96eaddb0be83a,
   "/components/admin/IndustrialNavLink.tsx#default": default_16641a84a1b15c8d66ac7b55d014767f,
+  "@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler": VercelBlobClientUploadHandler_16c82c5e25f430251a3e3ba57219ff4e,
   "/components/admin/Estate2Dashboard.tsx#default": default_1b588679deafe4663065ec587b5c83cf,
   "/components/admin/DholeraEstatesDashboard.tsx#default": default_d91c6f7e463ebcc16c96f2acdf19f207,
   "/components/admin/IndustrialDashboard.tsx#default": default_ff499614a2df010e10c2f729b61add75,
